b501155c75d1b1a9ec164a7a07cde76faf0117cd
teddyrugged <jakpaetuk@gmail.com>
2023-02-15 10:45:08 +0100

    updated_details

---
+++ walletApp/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+"""
+    We are using the default User model provided by Django.
+"""
+# Create your models here.
+
+
+class Wallet(models.Model):
+    user = models.OneToOneField(
+        User, null=True, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=50, default='NGN')
+    created_at = models.DateTimeField(default=timezone.now, null=True)
+
+
+    def __str__(self):
+        return self.user.__str__()
+
+class WalletTransaction(models.Model):
+
+    TRANSACTION_TYPES = (
+        ('deposit', 'deposit'),
+        ('transfer', 'transfer'),
+        ('withdraw', 'withdraw'),
+    )
+    wallet = models.ForeignKey(Wallet, null=True, on_delete=models.CASCADE)
+    transaction_type = models.CharField(
+        max_length=200, null=True,  choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=100, null=True, decimal_places=2)
+    timestamp = models.DateTimeField(default=timezone.now, null=True)
+    status = models.CharField(max_length=100, default="pending")
+    paystack_payment_reference = models.CharField(max_length=100, default='', blank=True)
+
+    def __str__(self):
+        return self.wallet.user.__str__()
---
+++ walletApp/serializers.py
@@ -0,0 +1,82 @@
+
+from rest_framework import serializers
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from .models import Wallet, WalletTransaction
+from django.db.models import Sum
+from django.conf import settings
+import requests
+
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """
+    Serializer to validate and create a new user
+    """
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        Token.objects.create(user=user)
+        return user
+
+
+class WalletSerializer(serializers.ModelSerializer):
+    """
+    Serializers to validate the user's wallet
+    """
+    balance = serializers.SerializerMethodField()
+
+    def get_balance(self, obj):
+        bal = WalletTransaction.objects.filter(
+            wallet=obj, status="success").aggregate(Sum('amount'))['amount__sum']
+        return bal
+
+    class Meta:
+        model = Wallet
+        fields = ['id', 'currency', 'balance']
+
+
+def is_amount(value):
+    if value <= 0:
+        raise serializers.ValidationError({"detail": "Invalid Amount"})
+    return value
+
+
+class DepositSerializer(serializers.Serializer):
+
+    amount = serializers.IntegerField(validators=[is_amount])
+    email = serializers.EmailField()
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            return value
+        raise serializers.ValidationError({"detail": "Email not found"})
+
+    def save(self):
+        user = self.context['request'].user
+        wallet = Wallet.objects.get(user=user)
+        data = self.validated_data
+        url = 'https://api.paystack.co/transaction/initialize'
+        headers = {
+            {"authorization": f"Bearer {settings.PAYSTACK_SECRET_KEY}"}}
+        r = requests.post(url, headers=headers, data=data)
+        response = r.json()
+        WalletTransaction.objects.create(
+            wallet=wallet,
+            transaction_type="deposit",
+            amount= data["amount"],
+            paystack_payment_reference=response['data']['reference'],
+            status="pending",
+        )
+
+        return response
